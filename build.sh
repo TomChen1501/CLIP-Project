b1853c85a680e90584d17eddfbba5f5e710faa23
TomChen1501 <43362640+TomChen1501@users.noreply.github.com>
2025-04-26 06:48:01 +0800

    Test pre-deply run of Render Trial 5

--- build.sh
+++ build.sh
@@ -1,19 +1,29 @@
 mkdir -p Resource
 
-echo "Installing gdown..."
-pip install gdown
+echo "Current dir: $(pwd)"
+ls -lh Resource/
 
 echo "Downloading encoded_tensors.pt..."
-gdown --id 1Apj_3U8aEXQqr_2dBoE_TAJhzaB0vaY0 -O Resource/encoded_tensors.pt
+gdown 1Apj_3U8aEXQqr_2dBoE_TAJhzaB0vaY0 -O Resource/encoded_tensors.pt
+echo "Current dir: $(pwd)"
+ls -lh Resource/
 
 echo "Downloading all_image_embeddings.pt..."
-gdown --id 15z6Ah0EcbB_d6YTLaemYo8GHwrQPcNie -O Resource/all_image_embeddings.pt
+gdown 15z6Ah0EcbB_d6YTLaemYo8GHwrQPcNie -O Resource/all_image_embeddings.pt
+echo "Current dir: $(pwd)"
+ls -lh Resource/
 
 echo "Downloading list_attr_celeba.txt..."
-gdown --id 1FyDxSKdqfc3zbamWMyZxalGTpLZ70kfh -O Resource/list_attr_celeba.txt
+gdown 1FyDxSKdqfc3zbamWMyZxalGTpLZ70kfh -O Resource/list_attr_celeba.txt
+echo "Current dir: $(pwd)"
+ls -lh Resource/
 
 echo "Downloading img_align_celeba.zip..."
-gdown --id 1QoCujOf6xTGtXgasCZ_Fcp8e5tXLPMsA -O Resource/img_align_celeba.zip
+gdown 1QoCujOf6xTGtXgasCZ_Fcp8e5tXLPMsA -O Resource/img_align_celeba.zip
+echo "Current dir: $(pwd)"
+ls -lh Resource/
 
 echo "Unzipping img_align_celeba.zip..."
 unzip -o Resource/img_align_celeba.zip -d Resource/
+echo "Current dir: $(pwd)"
+ls -lh Resource/
